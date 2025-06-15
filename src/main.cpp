492c9cda0775749bee41ba65245d3caa034b2b4e
fairl <fairl@SHADOW-92OPN663>
2025-06-15 18:14:20 +0200

    fix(main): add snippet for caps mult and fix array chanceNone

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,25 @@
 // here too, if you want, but it isn't strictly required.
 // Finally, we check if our plugin can safely be loaded by F4SE.
 // If we can, we return true. Otherwise false.
+template <typename... Ts>
+void SetWeightForAll(RE::TESDataHandler* dataHandler)
+{
+    // Ensure all types inherit from RE::TESWeightForm
+    static_assert((std::is_base_of_v<RE::TESWeightForm, Ts> && ...),
+        "must inherit RE::TESWeightForm");
+
+    (([&]() {
+        auto& itemForms = dataHandler->GetFormArray<Ts>();
+        for (auto* item : itemForms) {
+            item->weight = 0;
+        }
+    }()),
+        ...);
+}
+
 extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info)
 {
-#ifndef NDEBUG
+#if false
 	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
 #else
 	auto path = logger::log_directory();
@@ -57,18 +73,40 @@ extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f
 	F4SE::GetMessagingInterface()->RegisterListener([](F4SE::MessagingInterface::Message* message) {
 		if (message->type == F4SE::MessagingInterface::kGameDataReady) {
 			auto* dataHandler = RE::TESDataHandler::GetSingleton();
+            SetWeightForAll<RE::IngredientItem, RE::AlchemyItem, RE::TESAmmo, RE::TESObjectLIGH, RE::TESObjectMISC, RE::TESObjectBOOK>(dataHandler);
 			auto& forms = dataHandler->GetFormArray<RE::TESLevItem>();
+            auto& armors = dataHandler->GetFormArray<RE::TESObjectARMO>();
+			auto& weaps = dataHandler->GetFormArray<RE::TESObjectWEAP>();
+            for (auto* weap : weaps) {
+                weap->weaponData.weight = 0;
+            }
+            for (auto* armo : armors) {
+                armo->data.weight = 0;
+            }
 			for (auto* lvlform : forms) {
 				if (lvlform->chanceNone < 100) {
-					logger::info("patching the form : {0}", lvlform->GetFormID());
+                    if (lvlform->chanceNone > 0)
+					    logger::info("patching the form : {0} with chanceNone > 0", lvlform->GetFormID());
 					lvlform->chanceNone = 0;
 				}
-                if (lvlform->leveledLists && lvlform->leveledLists->chanceNone < 100)
-                    lvlform->leveledLists->chanceNone = 0;
+                if (lvlform->leveledLists) {
+                    for (int i = 0; i < lvlform->baseListCount; ++i) {
+                        auto& obj = lvlform->leveledLists[i];
+                        //todo add if chance < 100
+                        obj.chanceNone = 0;
+                        if (obj.form && obj.form->formID == 0xF) {
+                            obj.count *= 10;
+                        }
+                    }
+                }
+                   
                 if (lvlform->scriptAddedLists) {
                     for (int i = 0; i < lvlform->scriptListCount; i++) {
                         if (lvlform->scriptAddedLists[i]->chanceNone < 100)
                             lvlform->scriptAddedLists[i]->chanceNone = 0;
+                        if (lvlform->scriptAddedLists[i]->form->formID == 0xF) {
+                            lvlform->scriptAddedLists[i]->count *= 10;
+                        }
                     }
                 }
 			}
